8974c015bb2dd1f67a83961828915cd5591fac1b
HeladiaSalgado <heladia@ccg.unam.mx>
2023-04-17 10:15:53 -0600

    head and variables name updates
    - cuida los nombres de las variables, los nombres deben reflejar lo que guardan
    - trabaja en el encabezado
    - Para las pruebas recomiendo un archivo aparte, aunque si quieres incluirlo en el programa, mejor que vaya en el encabezado.

--- tareas/T4-RAW_FASTA.py
+++ tareas/T4-RAW_FASTA.py
@@ -1,41 +1,53 @@
 """
-Tarea 4
+NAME
+     T4-RAW_FASTA.py
+     
+VERSION
+        1.0
+AUTHOR
+        José Antonio Sánchez Villicaña
+        
+DESCRIPTION
 
-**Problema:**
-
-Crear un archivo fasta a partir de la secuencia de DNA que está en `dna.txt`
-
-Ejemplo de un archivo fasta:
-
-```
+        Crea un archivo fasta a partir de la secuencia de DNA en formato raw
+      
+      input file (raw format)
+ATCGACTGATCGATCGTACGAT
+      
+      output file (fastA format)
 > sequence_name
 ATCGACTGATCGATCGTACGAT
+
+        
+CATEGORY
+        Genómica
+USAGE
+    % py T4-RAW_FASTA.py
+    
+TESTING
+
+  github url input file: python1\dna.txt
+  
+
 """
-#Autor: José Antonio Sánchez Villicaña
 
+# Nombre del archivo de entrada con la secuencia en formato raw
+input_file_name = input('Escribe ruta y nombre archivo con la secuencia de DNA: ')
 
-#Ruta a utilizar para pruebas
-#C:\Users\Pepe S\Documents\Genomicas\2doSemestre\Python\python1\dna.txt
+# Nombre del archivo de salida
+output_file_name = input('Escribe el nombre del archivo de salida (con extensión .fasta): ')
 
-#Ruta del archivo con la secuencia
-input_file = input('Escribe la ruta del archivo con la secuencia de DNA: ')
+#Leer al archivo con la secuencia en formato raw
+input_file = open(input_file_name)
+dna_seq_raw = input_file.read()
+input_file.close()
 
-#Nombre del archivo de salida
-output_file = input('Escribe el nombre del archivo de salida (con extensión .fasta): ')
+# Crear archivo de salida
+output_file = open(output_file_name, 'w')
 
-#Accedo al archivo con la secuencia
-my_file = open(input_file)
-
-#Leo el contenido
-my_file_content = my_file.read()
-my_file.close()
-
-#Abro el archivo que se va a generar, con w para que reescriba lo que tenga si es que existe
-new_file = open(output_file, 'w')
-#Introduzco el formato deseado antes de la secuencia
-new_file.write('> sequence_name\n')
-#Introduzco la secuencia obtenida a partir del archivo introducido por el usuario
-new_file.write(my_file_content)
-new_file.close()
+# Formateando secuencia a fastA y guardando en el archivo de salida
+output_file.write('> sequence_name\n')
+output_file.write(dna_seq_raw)
+output_file.close()
 
 print('Archivo convertido exitosamente')
