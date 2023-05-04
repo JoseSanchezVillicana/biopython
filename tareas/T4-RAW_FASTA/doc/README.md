4551251384a97d3a9fc22ef443577dba40e6e7f9
José Antonio Sánchez Villicaña <josesv@lcg.unam.mx>
2023-05-03 22:28:06 -0600

    Tarea 4 corregida y con los elementos try agregado

---
+++ tareas/T4-RAW_FASTA/doc/README.md
@@ -0,0 +1,22 @@
+# José Antonio Sánchez Villicaña
+## Tarea 4 - Programación en Python
+### Antecedentes
+La finalidad de este programa es poner en práctica las sentencias de manejo de archivos, como utilizar una ruta absoluta para acceder a un archivo, extraer su contenido, crear un archivo, escribir en un archivo, etc.
+
+### Problema
+El programa va a recibir un archivo de texto con una secuencia de DNA y lo va a convertir en un archivo tipo fasta con esa secuencia.
+
+### Metodología
+1. Pedimos la ruta absoluta del archivo de texto con la secuencia de DNA
+2. Pedimos la ruta absoluta del directorio donde vamos a guardar el archivo fasta convertido
+3. Pedimos el nombre del archivo de salida (sin la extensión fasta)
+4. Abrimos el archivo de texto, extraemos el contenido
+5. Creamos el archivo fasta, le damos el formato fasta, agregamos el contenido del archivo de texto
+6. Cerramos archivos
+
+### Observaciones
+Por algún motivo no me dejaba meter la sección de TESTING en en el encabezado, me aparecia el error **'SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 1370-1371: truncated \UXXXXXXXX escape'** y sólo pude resolverlo poniendo la sección fuera del encabezado. Intenté poner la ruta entre comillas, mover la sección en la parte de en medio del encabezado, nada me funcionó excepto sacar la sección por completo.
+
+### Resultados
+
+Imagen adjunta en carpeta doc de esta tarea.
--- tareas/T4-RAW_FASTA/src/T4-RAW_FASTA.py
+++ tareas/T4-RAW_FASTA/src/T4-RAW_FASTA.py
@@ -1,41 +1,92 @@
-"""
-Tarea 4
+'''
+NAME
+        Archivo RAW a FASTA       
 
-**Problema:**
+VERSION
+        2.0
 
-Crear un archivo fasta a partir de la secuencia de DNA que está en `dna.txt`
+AUTHOR
+        José ANtonio Sánchez Villicaña
 
-Ejemplo de un archivo fasta:
+DESCRIPTION
+        Crear un archivo fasta a partir de la secuencia en un archivo de texto
+        Se modifica para que tenga el formato deseado
 
-```
-> sequence_name
-ATCGACTGATCGATCGTACGAT
-"""
-#Autor: José Antonio Sánchez Villicaña
+        Ejemplo de un archivo fasta:
+
+        > sequence_name
+        ATCGACTGATCGATCGTACGAT
+
+CATEGORY
+        Convertidor de archivos
+
+USAGE
+
+        Se tiene que proveer la ruta absoluta del archivo de texto con la secuencia de DNA
+        Se tiene que proveer la ruta absoluta de la carpeta donde se quiere guardar el archivo generado
+        Se tiene que proveer el nombre (sin la extensión fasta) del archivo a generar 
 
 
-#Ruta a utilizar para pruebas
-#C:\Users\Pepe S\Documents\Genomicas\2doSemestre\Python\python1\dna.txt
+ARGUMENTS
+        input_file = ruta del archivo de texto con la secuencia de DNA
+        my_file = variable del archivo de texto con la secuencia
+        my_file_content = variable que almacena el contenido del archivo de texto (la secuencia de DNA)
+        target_dir = ruta del directorio donde se quiere guardar el archivo convertido
+        output_file = nombre del archivo nuevo a generar (sin extensión .fasta)
+        new_file = variable del archivo nuevo al que vamos a agregar el contenido del archivo de texto con el formato deseado
+        
+'''
+#TESTING
+        #Ruta de archivo de texto a utilizar para pruebas -> #C:\Users\Pepe S\Documents\Genomicas\2doSemestre\Python\python1\tareas\T4-RAW_FASTA\data\dna.txt
+        #Ruta donde queremos guardar el archivo final -> #C:\Users\Pepe S\Documents\Genomicas\2doSemestre\Python\python1\tareas\T4-RAW_FASTA\doc
+        #Para la prueba se está guardando el archivo generado en la carpeta doc de esta tarea
+        #En el README de esta tarea se explica por qué esta sección de testing no está en el encabezado
 
-#Ruta del archivo con la secuencia
-input_file = input('Escribe la ruta del archivo con la secuencia de DNA: ')
 
-#Nombre del archivo de salida
-output_file = input('Escribe el nombre del archivo de salida (con extensión .fasta): ')
+# ===========================================================================
+# =                            imports
+# ===========================================================================
+import os
+
+
+# ===========================================================================
+# =                             main
+# ===========================================================================
 
 #Accedo al archivo con la secuencia
-my_file = open(input_file)
+input_file = input('Escribe la ruta del archivo con la secuencia de DNA: ')
+try:
+    my_file = open(input_file)
+except (FileNotFoundError):
+    print('ERROR Ruta invalida')
+    quit()
 
 #Leo el contenido
 my_file_content = my_file.read()
 my_file.close()
 
-#Abro el archivo que se va a generar, con w para que reescriba lo que tenga si es que existe
-new_file = open(output_file, 'w')
-#Introduzco el formato deseado antes de la secuencia
-new_file.write('> sequence_name\n')
-#Introduzco la secuencia obtenida a partir del archivo introducido por el usuario
-new_file.write(my_file_content)
-new_file.close()
+#Obtengo ruta de la carpeta donde se guardará el archivo y me muevo ahí
+target_dir = input('Ingresa la ruta de la carpeta donde se guardará el archivo convertido: ')
+try:
+    os.chdir(target_dir)
+except (FileNotFoundError):
+    print('ERROR Ruta invalida')
+    quit()
 
-print('Archivo convertido exitosamente')
+#Nombre del archivo de salida
+output_file = input('Escribe el nombre del archivo de salida (sin la extensión .fasta): ')
+output_file += '.fasta'
+
+#Generación del nuevo archivo en formato fasta
+try:
+    #Abro el archivo que se va a generar, con w para que reescriba lo que tenga si es que existe
+    new_file = open(output_file, 'w')
+    #Introduzco el formato deseado antes de la secuencia
+    new_file.write('> sequence_name\n')
+    #Introduzco la secuencia obtenida a partir del archivo introducido por el usuario
+    new_file.write(my_file_content)
+    new_file.close()
+except:
+    print(f'ERROR al intentar crear el archivo {output_file}')
+else:
+    print('Archivo convertido exitosamente')
